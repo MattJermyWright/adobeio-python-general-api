ad67c6b094d8012acd1d06dc2904e9951ec47f8d
Matt Wright <matt.jeremy.wright@gmail.com>
2019-07-31 22:41:33 -0600

    Updated test cases

--- py_adobeio_api/tests/test_basic_spec.py
+++ py_adobeio_api/tests/test_basic_spec.py
@@ -1,4 +1,12 @@
-from libs.adobe_rest import load_config_file
+from libs.adobe_rest import load_config_file, AdobeIORestAPI
 
-def test_api_retriever():
-    assert True
+def test_config_loader():
+    loaded_file = load_config_file("config.json")
+    assert "launch" in loaded_file
+
+def test_company_listing():
+    loaded_config_file = load_config_file("config.json")
+    rest_api = AdobeIORestAPI(loaded_config_file)
+    response = rest_api.rest_get("https://reactor.adobe.io/companies")
+    assert "data" in response
+    assert len(response["data"])>0
